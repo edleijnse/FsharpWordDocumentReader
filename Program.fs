8f27efd9d89d4f2c7ab96673ae9afa0fb34dbb81
Ed Leijnse <ed@leijnse.info>
2022-03-27 21:53:06 +0200

    read Content of Word Document, Version 1.0

--- Program.fs
+++ Program.fs
@@ -1,6 +1,8 @@
 open System;
 open System.Collections.Generic
 open System.IO;
+open System.IO.Packaging;
+open System.Xml;
  
 // let applicationDirectory = Environment.CurrentDirectory;
 let applicationDirectory = "F:\swissedu"
@@ -11,8 +13,22 @@ let rec getAllFilesNames directory =
           for d in Directory.EnumerateDirectories(directory) do yield! getAllFilesNames d}
  
 let outFile = new StreamWriter(applicationDirectory + @"\swissedu_documents.txt")
+let getDocxContent (path: string) =
+    use package = Package.Open(path, FileMode.Open)
+    let stream = package.GetPart(new Uri("/word/document.xml", UriKind.Relative)).GetStream()
+    stream.Seek(0L, SeekOrigin.Begin) |> ignore
+    let xmlDoc = new XmlDocument()
+    xmlDoc.Load(stream)
+    xmlDoc.DocumentElement.InnerText
  
 seq{for d in Directory.EnumerateDirectories(applicationDirectory) do yield! getAllFilesNames d} 
-|> Seq.iter (fun (x:string) -> outFile.WriteLine(x.Substring(applicationDirectory.Length+1))) 
+|> Seq.iter (fun (x:string) ->
+    outFile.WriteLine("---------------------------------------------------")
+    outFile.WriteLine(x.Substring(applicationDirectory.Length+1))
+    outFile.WriteLine(getDocxContent(x))
+    outFile.WriteLine("---------------------------------------------------")
+    )
  
 outFile.Close()
+    
+printfn "%s" (getDocxContent @"F:\swissedu\swissedu_attachments\2020-09-21_Lesson 1- notes.docx")   
